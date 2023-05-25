c6632bc2c663e940295354181829cc593d7b2c2c
hemuu12 <harisingh.bisht2001@gmail.com>
2023-05-25 08:55:57 +0530

    add

---
+++ routers/post.router.js
@@ -0,0 +1,80 @@
+const express=require("express")
+
+const {postModel}=require("../models/post.model")
+
+const postRouter=express.Router()
+
+
+// Get Operation
+
+postRouter.get("/",async(req,res)=>{
+
+    try{
+        let data
+        data=await postModel.find()
+        res.status(200);
+        res.send({
+            status: 200,
+            data: data,
+        })
+    }
+    catch(error){
+        res.status(400);
+        res.send({ status: 400, error: error });
+
+
+    }
+
+
+
+})
+
+
+// Post Operation
+
+postRouter.post("/addemployee",async(req,res)=>{
+    const data=req.body
+    try {
+        let newdata=new postModel(data)
+        newdata.save()
+        res.send({status: 200, data:"post is been added"})
+    } catch (error) {
+        res.send({status: 400, error: error})
+    }
+})
+
+
+// update operation
+
+
+postRouter.put("/update/:id",async(req,res)=>{
+    let Id=req.params.id
+    const data=req.body
+    try {
+        await postModel.findByIdAndUpdate({_id:Id},data)
+        res.send({status: 200, Data:"post is been updated"})
+    } catch (error) {
+        res.send({status: 400, error: error})
+    }
+})
+
+// delete operation
+
+postRouter.delete("/delete/:id",async(req,res)=>{
+    let Id=req.params.id
+    try {
+        await postModel.findByIdAndDelete({_id:Id})
+        res.send({status: 200,data:"post is been deleted"})
+    } catch (error) {
+        res.send({status: 400, error: error})
+    }
+})
+
+
+
+
+
+
+module.exports={
+    postRouter
+}
